f9ba3f9aa3b76c8232cadeead584fd7853568934
CristianDimuro <123915703+CristianDimuro@users.noreply.github.com>
2023-01-29 23:49:52 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,44 @@
+function FunctionInfo() {
+    var x = document.getElementById("info");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function FunctionEdu() {
+    var x = document.getElementById("edu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function FunctionExp() {
+    var x = document.getElementById("exp");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }  
+
+  function FunctionHab() {
+    var x = document.getElementById("hab");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function FunctionCont() {
+    var x = document.getElementById("cont");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
